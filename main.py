c5f809bd0065b60835cd6b823a56f060e9729cca
Erwann <e.chinal@aviwell.fr>
2025-08-27 11:35:08 +0200

    Initial commit and environement setup

---
+++ main.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, Field
+from langchain.chat_models import init_chat_model
+import numpy as np
+import pandas as pd
+
+def main():
+    print("Hello from genemind!")
+
+
+if __name__ == "__main__":
+    main()
